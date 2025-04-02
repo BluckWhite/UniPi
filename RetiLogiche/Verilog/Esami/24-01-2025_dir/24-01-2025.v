9b2af7988aa1273479642fb07369d303ce3d5314
Nick <nicholasdelbuono2004@gmail.com>
2025-04-02 22:23:56 +0200

    Modifica esame verilog

--- RetiLogiche/Verilog/Esami/24-01-2025_dir/24-01-2025.v
+++ RetiLogiche/Verilog/Esami/24-01-2025_dir/24-01-2025.v
@@ -1,6 +1,6 @@
 module ABC(soc1, eoc1, x1,
            soc2, eoc2, x2,
-           out, reset_, clock)
+           out, reset_, clock);
 
     output soc1, soc2;
     reg SOC;
@@ -33,7 +33,7 @@ module ABC(soc1, eoc1, x1,
     always @(posedge clock) if(reset_ == 1) begin
         casex(STAR)
             S0: begin 
-                OUT <= 0;
+                #3 OUT <= 0;
                 SOC <= 1;
                 STAR <= ( {eoc1, eoc2} == 2'B00 ) ? S1 : S0;
             end
@@ -43,22 +43,25 @@ module ABC(soc1, eoc1, x1,
                 COUNT <= media;
             end
             S2: begin
+                STAR <= (COUNT == 0) ? S0 : S3;
+            end
+            S3: begin                
                 OUT <= 1;
                 COUNT <= COUNT - 1;
-                STAR <= (COUNT == 1) ? S0 : S2;
+                STAR <= (COUNT == 1) ? S0 : S3;
             end
         endcase
     end
 endmodule
 
-module MEDIA(x1, x2, m)
+module MEDIA(x1, x2, m);
 
     input[7:0] x1, x2;
     output[7:0] m;
 
     wire[8:0] s;
 
-    add a(.x(x1),.y(x2),.c_in(1'B0),.c_out(s[8]),.s(s[7:0]));
+    add #(.N(8)) a(.x(x1),.y(x2),.c_in(1'B0),.c_out(s[8]),.s(s[7:0]));
     assign m = s[8:1];
 
 endmodule
